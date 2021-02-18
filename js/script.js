c08ccd1b906bcaa4d0d4683b5002a8a01e09c10b
Vladimir <vladimir.brims@mail.ru>
2021-02-19 00:38:34 +0300

    add const num if/else

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 'use strict';
-  const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [],
-privat: false
-};
-const a = prompt('Один из последних просмотренных вильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных вильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+//   const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {
+// count: numberOfFilms,
+// movies: {},
+// actors: {},
+// genres: [],
+// privat: false
+// };
+// const a = prompt('Один из последних просмотренных вильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных вильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
- personalMovieDB.movies[a] = b;  
- personalMovieDB.movies[c] = d;  
+//  personalMovieDB.movies[a] = b;  
+//  personalMovieDB.movies[c] = d;  
  
- console.log(personalMovieDB);
+//  console.log(personalMovieDB);
+
+// if (4==4) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+const num = 50;
+if (num<49) {
+    console.log('Error');
+} else if (num>100) {
+    console.log('много!');
+} else {
+    console.log('Ok');
+}
