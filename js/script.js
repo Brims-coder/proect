94955940e6711e2a1274bed59d4425c7551d6283
Vladimir <vladimir.brims@mail.ru>
2021-02-28 20:28:36 +0300

    add copy

--- js/script.js
+++ js/script.js
@@ -168,3 +168,39 @@ genres
 P.S. Функции вызывать не обязательно*/
 
 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+ 
+ for (let i = 1; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных вильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+         
+
+          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+          } 
+else {
+    console.log('error');
+    i--;
+   
+}
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+ } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+ } else if (personalMovieDB.count >= 30) {
+console.log ("Вы киноман");
+ }
+else {
+  console.log("Произошла ошибка");
+}
+ console.log(personalMovieDB);
