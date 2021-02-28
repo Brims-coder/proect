29f8bc3b7112871a74dbba100ab21a72ae03f4b9
Vladimir <vladimir.brims@mail.ru>
2021-02-28 21:01:15 +0300

    add function rememberMyFilms

--- js/script.js
+++ js/script.js
@@ -185,20 +185,23 @@ genres: [],
 privat: false
 };
  
- for (let i = 1; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных вильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-         
-
-          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-          } 
-else {
-    console.log('error');
-    i--;
-   
-}
+ 
+function rememberMyFilms () {
+    for (let i = 1; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных вильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+             
+    
+              if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+              } 
+    else {
+        console.log('error');
+        i--;
+       
+    }
+    }
 }
 
 if (personalMovieDB.count < 10) {
